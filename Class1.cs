8b695cefea790302dc7df64e5a4e7b243ddd173a
PavelVorobyev77 <pascha27_05@mail.ru>
2024-01-26 22:45:41 +0700

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathTaskClassLibrary
+{
+    public class Geometry
+    {
+        public int RectangleArea(int a, int b)
+        {
+            return a * b;
+        }
+
+        public double CylinderVolume(double radius, double height)
+        {
+            return Math.PI * Math.Pow(radius, 2) * height; //V = π * r^2 * h
+        }
+
+        public double CircleDiameter(double radius)
+        {
+            return 2 * radius; //D = 2 * r
+        }
+
+        public double CircleRadius(double area)
+        {
+            return Math.Sqrt(area / Math.PI); //r = √(S / π)
+        }
+
+        public double PyramidVolume(double baseArea, double height)
+        {
+            return baseArea * height / 3; //V = (1/3) * S * h
+        }
+        public double CircleCircumference(double radius)
+        {
+            return 2 * Math.PI * radius; //C = 2 * π * r
+        }
+
+    }
+}
